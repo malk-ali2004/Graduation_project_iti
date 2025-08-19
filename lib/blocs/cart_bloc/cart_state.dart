b0f121aa03f0e4816fcf460c802087d915e72b60
malk-ali2004 <ma6661@fayoum.edu.eg>
2025-08-19 22:50:56 +0300

    UPDATE OF CART for firebzase

--- lib/blocs/cart_bloc/cart_state.dart
+++ lib/blocs/cart_bloc/cart_state.dart
@@ -1,32 +1,4 @@
-// import 'package:equatable/equatable.dart';
-// import '../../models/product.dart';
 
-// class CartItem extends Equatable {
-//   final Product product;
-//   final int quantity;
-
-//   const CartItem({required this.product, this.quantity = 1});
-
-//   CartItem copyWith({int? quantity}) {
-//     return CartItem(product: product, quantity: quantity ?? this.quantity);
-//   }
-
-//   @override
-//   List<Object> get props => [product, quantity];
-// }
-
-// class CartState extends Equatable {
-//   final List<CartItem> items;
-
-//   const CartState({this.items = const []});
-
-//   CartState copyWith({List<CartItem>? items}) {
-//     return CartState(items: items ?? this.items);
-//   }
-
-//   @override
-//   List<Object> get props => [items];
-// }
 
 import 'package:equatable/equatable.dart';
 import '../../models/product.dart';
