252fde1bdf0f89c85b0d9a2c6f81331a0960dfef
malk-ali2004 <ma6661@fayoum.edu.eg>
2025-08-19 21:59:44 +0300

    final update cart

--- lib/blocs/cart_bloc/cart_event.dart
+++ lib/blocs/cart_bloc/cart_event.dart
@@ -1,46 +1,4 @@
-// import 'package:equatable/equatable.dart';
-// import '../../models/product.dart';
 
-// abstract class CartEvent extends Equatable {
-//   @override
-//   List<Object> get props => [];
-// }
-
-// class AddToCart extends CartEvent {
-//   final Product product;
-
-//   AddToCart(this.product);
-
-//   @override
-//   List<Object> get props => [product];
-// }
-
-// class RemoveFromCart extends CartEvent {
-//   final Product product;
-
-//   RemoveFromCart(this.product);
-
-//   @override
-//   List<Object> get props => [product];
-// }
-
-// class IncreaseQuantity extends CartEvent {
-//   final Product product;
-
-//   IncreaseQuantity(this.product);
-
-//   @override
-//   List<Object> get props => [product];
-// }
-
-// class DecreaseQuantity extends CartEvent {
-//   final Product product;
-
-//   DecreaseQuantity(this.product);
-
-//   @override
-//   List<Object> get props => [product];
-// }
 
 import 'package:equatable/equatable.dart';
 import '../../models/product.dart';
